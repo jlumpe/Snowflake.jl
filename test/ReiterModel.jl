da41b0b1293b0256c9a03615e02074184b5d3228
Jared Lumpe <jared@jaredlumpe.com>
2024-12-22 19:01:15 -0800

    Update tests

--- test/ReiterModel.jl
+++ test/ReiterModel.jl
@@ -1,5 +1,5 @@
 using Snowflake: ReiterModel, frozen, boundary, nonreceptive
-using Snowflake: init!, refresh_ct!
+using Snowflake: init!, update!, refresh_ct!
 
 
 @testset "init" begin
@@ -8,6 +8,7 @@ using Snowflake: init!, refresh_ct!
 
 	n = 3
 	init!(model, n)
+	@test model.radius == n
 
 	for ix in model.shape
 		d = hexdist(ix)
@@ -22,4 +23,63 @@ using Snowflake: init!, refresh_ct!
 			@test model.ct[ix] == nonreceptive
 		end
 	end
+end
+
+
+@testset "run" begin
+	params = (α=2, β=.4, γ=.001)
+	model = ReiterModel(30, params)
+	init!(model)
+
+	for _ in 1:50
+		update!(model)
+	end
+
+	# Check cells
+	for z in model.shape
+		ct = model.ct[z]
+		s = model.s[z]
+
+		# Check frozen based on value of s
+		@test (ct == frozen) == (s >= 1)
+
+		# Check neighbors
+		has_frozen_neighbor = false
+		for zn in neighbors(model.shape, z)
+			ctn = model.ct[zn]
+
+			# Test neighbors of frozen are frozen or boundary
+			if ct == frozen
+				@test ctn in (frozen, boundary)
+			end
+
+			ctn == frozen && (has_frozen_neighbor = true)
+		end
+
+		# Boundary cells must have frozen neighbor
+		if ct == boundary
+			@test has_frozen_neighbor
+		end
+	end
+end
+
+
+@testset "refresh_ct!()" begin
+	params = (α=2, β=.4, γ=.001)
+	model = ReiterModel(30, params)
+	init!(model)
+
+	for _ in 1:50
+		update!(model)
+	end
+
+	old_ct = copy(model.ct)
+	old_radius = model.radius
+
+	fill!(model.ct, nonreceptive)
+	model.radius = 0
+	refresh_ct!(model)
+
+	@test model.ct == old_ct
+	# @test model.radius == old_radius
 end
